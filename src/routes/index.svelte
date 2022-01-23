0d5f93184035ff99f684630f1a1de9bc2bc65daf
shantanukr <mail2shantanukumar@gmail.com>
2022-01-23 06:55:30 +0000

    Adding projct-card component

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,7 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <script context="module">
+  import ProjectCard from '$lib/components/project-card.svelte'
   import { client } from '$lib/graphql-client'
   import { gql } from 'graphql-request'
 
@@ -35,4 +36,11 @@
   export let projects
 </script>
 
-<pre>{JSON.stringify(projects, null, 2)}</pre>
+
+<h1>Recent Changes by Me</h1>
+
+<div>
+  {#each projects as { name, slug, description, image }}
+    <ProjectCard {name} {description} url={image[0].url} {slug} />
+  {/each}
+</div>
