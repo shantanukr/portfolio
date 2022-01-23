bcfd4b12a14bd363ee6692ae56f4be8dffb0a044
shantanukr <mail2shantanukumar@gmail.com>
2022-01-23 07:17:12 +0000

    welcome page with projects listed

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,30 +3,22 @@
 <script context="module">
   import ProjectCard from '$lib/components/project-card.svelte'
   import { client } from '$lib/graphql-client'
-  import { gql } from 'graphql-request'
+  import { authorsQuery, projectsQuery } from '$lib/graphql-queries'
 
   export const load = async () => {
 
-      const query = gql`
-      query GetProjects {
-        projects {
-          name
-          slug
-          description
-          demo
-          sourceCode
-          image {
-            url
-          }
-        }
-      }
-    `
+    const [authorReq, projectsReq] = await Promise.all([
+      client.request(authorsQuery),
+      client.request(projectsQuery),
+    ])
 
-    const { projects } = await client.request(query)
+    const { authors } = authorReq
+    const { projects } = projectsReq
 
     return {
       props: {
         projects,
+        authors,
       },
     }
   }
@@ -34,10 +26,28 @@
 
 <script>
   export let projects
+  export let authors
 </script>
 
+<svelte:head>
+  <title>My Portfolio project</title>
+</svelte:head>
 
-<h1>Recent Changes by Me</h1>
+
+<h1 class="font-bold text-center mb-20 text-5xl"> Welcome to my Portfolio </h1>
+
+{#each authors as { name, intro, picture: { url } }}
+  <div class="flex mb-40 items-end">
+    <div class="mr-6">
+      <h2 class="text-3xl mb-4 font-bold tracking-wider">{name}</h2>
+      <p class="text-xl mb-4">{intro}</p>
+    </div>
+
+    <img class="mask mask-squircle h-48" src={url} alt={name} />
+  </div>
+{/each}
+
+<div class="grid gap-10 md:grid-cols-4 md:px-10 lg:grid-cols-6 lg:-mx-52"></div>
 
 <div>
   {#each projects as { name, slug, description, image }}
